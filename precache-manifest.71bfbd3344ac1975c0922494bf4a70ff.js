d73f736fd06f5e20a8e60ff1569cc7a0500da784
romanca <r.jamrisko@gmail.com>
2021-07-24 10:50:05 +0200

    Updates

---
+++ precache-manifest.71bfbd3344ac1975c0922494bf4a70ff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3965697526e4c5bca075c84dcd9221fe",
+    "url": "/React-simple-calculator/index.html"
+  },
+  {
+    "revision": "dabda7bdabfebd8ae0f5",
+    "url": "/React-simple-calculator/static/css/main.2469d77b.chunk.css"
+  },
+  {
+    "revision": "f09a225acec79357b5d7",
+    "url": "/React-simple-calculator/static/js/2.8846cfc3.chunk.js"
+  },
+  {
+    "revision": "dabda7bdabfebd8ae0f5",
+    "url": "/React-simple-calculator/static/js/main.fd752476.chunk.js"
+  },
+  {
+    "revision": "75e64fdb73461f7379ef",
+    "url": "/React-simple-calculator/static/js/runtime~main.3d068aa2.js"
+  }
+]);
